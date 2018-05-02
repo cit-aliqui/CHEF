c2265b6e3ee633544f3cf46570f3e8f2217d626a
Raghu K <raghuk.vit@gmail.com>
2018-05-02 02:37:33 +0000

    Updating CHEF

--- cookbooks/mariadbdata/recipes/default.rb
+++ cookbooks/mariadbdata/recipes/default.rb
@@ -1,7 +1,19 @@
 DB = data_bag_item('sample', 'mariadb' )
-puts "#{DB['DBNAME']}"
+#puts "#{DB['DBNAME']}"
 #puts "#{DB[:DBNAME]}"
 template 'Copy sample sql file' do 
   path '/tmp/sample.sql'
-  source 'demo.erb'
+  source 'sample.sql.erb'
+  variables(
+        DBNAME: DB['DBNAME'],
+     	DBUSER: DB['DBUSER'],
+	DBPASS: DB['DBPASS']
+)
+end
+
+
+#node['fqdn'] = 'latte'
+template '/tmp/foo' do
+  source 'foo.erb'
+  variables(x_men: 'are keen')
 end
--- roles/stack.rb
+++ roles/stack.rb
@@ -1,3 +1,4 @@
 name "stack"
 description "Stack of DB +APP + WEB"
-run_list "recipe[httpd::install]","recipe[tomcat::install]","recipe[tomcat::service]","recipe[mariadb::install]","recipe[mariadb::config]"
+#run_list "recipe[httpd::install]","recipe[tomcat::install]","recipe[tomcat::service]","recipe[mariadb::install]","recipe[mariadb::config]"
+env_run_lists "prod" => "recipe[httpd]", "dev" => "recipe[mariadb]", "_default"  => []
